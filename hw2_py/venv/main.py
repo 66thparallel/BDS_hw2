489172e4258249eaa2879a2397df5c248a822ecf
Jane Liu <janeikeliu@gmail.com>
2019-03-13 09:22:41 -0400

    Add python files.

---
+++ hw2_py/venv/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+Authors Jane Liu and Meng Li
+
+Classes:
+
+"""
+
+import logging
+import unittest
+
+
+def main():
+
+    # Read file and store in a list
+    with open('src/C1/article01.txt', 'r') as f:
+        article = f.read().splitlines()
+
+    with open('src/stopwords.txt', 'r') as g:
+        stopwords = g.read().splitlines()
+
+    # Contains text without stop words
+    cleantxt = []
+
+    for item in article:
+        data = item.split()
+        for i in data:
+            cleantxt.append(i)
+
+    for articleword in cleantxt:
+        for stopword in stopwords:
+            if articleword == stopword:
+                cleantxt.pop(articleword)
+
+    # Test if all stop words have been removed.
+    for j in cleantxt:
+        print(j)
+
+main()
+
