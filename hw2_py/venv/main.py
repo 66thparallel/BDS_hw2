a2226c3a7ae545d0b465f0c63c1855f7b64bcc90
Jane Liu <janeikeliu@gmail.com>
2019-03-15 12:53:25 -0400

    Move preprocessing code from main to preprocessor.py.

--- hw2_py/venv/main.py
+++ hw2_py/venv/main.py
@@ -14,17 +14,6 @@ Notes (delete before submitting!!):
 import string
 import logging
 import unittest
-from collections import Counter
-
-import spacy
-from spacy import displacy
-import en_core_web_sm
-
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-
-# Preprocessing textual data
 from preprocessor import *
 
 
@@ -33,42 +22,11 @@ def main():
     with open('src/C1/article01.txt', 'r') as f:
         article = f.read().splitlines()
 
-    # Tokenize the text file
-    temptext = Tokenizer(article)
-    cleantext = temptext.tokenize()
+    Prep = Preprocessor(article)
 
-    # Remove stop words
-    for word in list(cleantext):
-        if word in stopwords.words('english'):
-            cleantext.remove(word)
+    topics = Prep.preprocess()
 
-    # Remove punctuation and empty strings
-    cleantext = [''.join(c for c in s if c not in string.punctuation) for s in cleantext]
-    cleantext = [s for s in cleantext if s]
-
-    # Lemmatize the text
-    lemma_text = []
-    lemmatizer = WordNetLemmatizer()
-
-    for word in list(cleantext):
-        new_word = lemmatizer.lemmatize(word)
-        lemma_text.append(new_word)
-
-    preprocessed_text = lemma_text
-
-    # Perform named-entity extraction
-    #nlp = en_core_web_sm.load()
-    #doc = nlp(' '.join(preprocessed_text))  # spaCy requires text to be in string form
-    #print([(X.text, X.label_) for X in doc.ents])
-
-    # Sliding windows
-
-
-    # Determine the frequency distribution of topics
-    frequency = nltk.FreqDist(preprocessed_text)
-
-    for key, val in frequency.items():
-        print(str(key) + ':' + str(val))
+    print(topics)
 
 
 main()
--- hw2_py/venv/preprocessor.py
+++ hw2_py/venv/preprocessor.py
@@ -7,16 +7,83 @@ Classes:
 
 """
 
+import string
+import logging
+import unittest
+
+import spacy
+from spacy import displacy
+import en_core_web_sm
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
 class Tokenizer:
 
-    def __init__(self, article):
-        self.article = article
-        self.tokenized_doc = []
+    def __init__(self, text):
+        self._text = text
+        self._tokenized_doc = []
 
     def tokenize(self):
-        for item in self.article:
+        for item in self._text:
             data = item.split()
             for i in data:
-                self.tokenized_doc.append(i)
+                self._tokenized_doc.append(i)
 
-        return self.tokenized_doc
+        return self._tokenized_doc
+
+
+class Preprocessor:
+
+    def __init__(self, article):
+        self._article = article
+        self._cleantext = []
+        self._temptext = []
+
+    def preprocess(self):
+
+        # Tokenize the text file
+        self._temptext = Tokenizer(self._article)
+        self._cleantext = self._temptext.tokenize()
+
+        # Remove stop words
+        for word in list(self._cleantext):
+            if word in stopwords.words('english'):
+                self._cleantext.remove(word)
+
+        # Remove punctuation and empty strings
+        self._cleantext = [''.join(c for c in s if c not in string.punctuation) for s in self._cleantext]
+        self._cleantext = [s for s in self._cleantext if s]
+
+        # Lemmatize the text
+        lemma_text = []
+        lemmatizer = WordNetLemmatizer()
+
+        for word in list(self._cleantext):
+            new_word = lemmatizer.lemmatize(word)
+            lemma_text.append(new_word)
+
+        preprocessed_text = lemma_text
+
+        # Perform named-entity extraction - need to combine the names
+        nlp = spacy.load('en_core_web_sm')
+        doc = nlp(' '.join(preprocessed_text))  # spaCy requires text to be in string form
+
+        # print out named entities
+        for ent in doc.ents:
+            print(ent.text, ent.label_)
+
+'''
+        # Sliding windows
+
+
+        # Determine the frequency distribution of topics
+        #frequency = nltk.FreqDist(preprocessed_text)
+
+        #for key, val in frequency.items():
+            print(str(key) + ':' + str(val))
+'''
+
+        return None
