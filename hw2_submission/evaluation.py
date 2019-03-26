ee64a9f5696790dce6457402b56744ec326153ac
Jane Liu <janeikeliu@gmail.com>
2019-03-25 23:30:34 -0400

    Add more instructions to README. In evaluation.py matrix instead of self._matrix was being called and causing error. Fixed.

--- hw2_submission/evaluation.py
+++ hw2_submission/evaluation.py
@@ -3,6 +3,6 @@ class evaluation:
     def __init__(self, matrix):
         self._matrix = matrix
     def eva(self):
-        matrix["actual_cluster"]=[2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
-        print(confusion_matrix(matrix["actual_cluster"],matrix['cluster']))
-        print(classification_report(matrix["actual_cluster"],matrix['cluster']))
+        self._matrix["actual_cluster"]=[2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
+        print(confusion_matrix(self._matrix["actual_cluster"],self._matrix['cluster']))
+        print(classification_report(self._matrix["actual_cluster"],self._matrix['cluster']))
